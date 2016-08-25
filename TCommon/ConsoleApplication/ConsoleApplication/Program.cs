985fdcc8ae9e1d00953ad2ba25dbf4c187d6d09f
Foskill <zhangjia517@hotmail.com>
2016-08-25 18:15:31 +0800

    LM

--- TCommon/ConsoleApplication/ConsoleApplication/Program.cs
+++ TCommon/ConsoleApplication/ConsoleApplication/Program.cs
@@ -6,13 +6,6 @@ namespace ConsoleApplication
     {
         private static void Main(string[] args)
         {
-            //TProcess.KillProcess();
-
-            //int[] t = new int[] { 8, 9, 10, 46, 15, 5, 23, 7, 1 };
-            //int a, b;
-            //TOutMax.MaxandMin(t, out a, out b);
-            //Console.WriteLine(a + " " + b);
-
             int i = 6;
             TOutMax.OutInt(i);
             Console.WriteLine(i);
