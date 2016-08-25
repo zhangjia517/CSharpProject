857b0ec87e48746a7d6179ceb15eb4ba3d2a4305
Foskill <zhangjia517@hotmail.com>
2016-08-25 18:12:38 +0800

    LM

--- TCommon/ConsoleApplication/ConsoleApplication/Program.cs
+++ TCommon/ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConsoleApplication
+﻿using System;
+
+namespace ConsoleApplication
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            TProcess.KillProcess();
+            //TProcess.KillProcess();
+
+            //int[] t = new int[] { 8, 9, 10, 46, 15, 5, 23, 7, 1 };
+            //int a, b;
+            //TOutMax.MaxandMin(t, out a, out b);
+            //Console.WriteLine(a + " " + b);
+
+            int i = 6;
+            TOutMax.OutInt(i);
+            Console.WriteLine(i);
         }
     }
 }
---
+++ TCommon/ConsoleApplication/ConsoleApplication/TOutMax.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApplication
+{
+    internal class TOutMax
+    {
+        public static void MaxandMin(int[] a, out int max, out int max2)
+        {
+            if (a[0] > a[1])
+            {
+                max = a[0];
+                max2 = a[1];
+            }
+            else
+            {
+                max = a[1];
+                max2 = a[0];
+            }
+
+            for (int i = 2; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max2 = max;
+                    max = a[i];
+                }
+                else if (a[i] > max2)
+                {
+                    max2 = a[i];
+                }
+            }
+        }
+
+        public static void OutInt(int i)
+        {
+            i = 5;
+        }
+    }
+}
