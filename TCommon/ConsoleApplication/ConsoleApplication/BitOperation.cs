db84cca16bc5193df3e7d92bc228e39d60c09371
Foskill <zhangjia517@hotmail.com>
2016-09-05 10:28:41 +0800

    LM

---
+++ TCommon/ConsoleApplication/ConsoleApplication/BitOperation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class BitOperation
+    {
+        public static void TDebug()
+        {
+            //int i = 1 << 2;
+            //Console.WriteLine(i);
+            //i = 1 << 4;
+            //Console.WriteLine(i);
+
+
+            int value = 4;
+            bool b = (value & 4) != 0;
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
--- TCommon/ConsoleApplication/ConsoleApplication/Program.cs
+++ TCommon/ConsoleApplication/ConsoleApplication/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            TJson.ToJ();
+            BitOperation.TDebug();
         }
     }
 }
