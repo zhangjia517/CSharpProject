3b6751c2a187aa9c91dda782264205d8dba8f4bc
Foskill <zhangjia517@hotmail.com>
2016-09-06 14:35:49 +0800

    LM

--- TCommon/ConsoleApplication/ConsoleApplication/BitOperation.cs
+++ TCommon/ConsoleApplication/ConsoleApplication/BitOperation.cs
@@ -16,11 +16,19 @@ namespace ConsoleApplication
             //Console.WriteLine(i);
 
 
-            int value = 4;
-            bool b = (value & 4) != 0;
-            Console.WriteLine(b);
+
+            //int value = 4;
+            //bool b = (value & 4) != 0;
+            //Console.WriteLine(b);
+
+            Console.WriteLine(getMinInt());
 
             Console.ReadKey();
+        }
+
+        static int getMinInt()
+        {
+            return ~6;//-2147483648
         }
     }
 }
