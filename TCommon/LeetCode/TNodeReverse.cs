7138bce1e4eb37d4adc9de13e85a58a4f54616f2
Foskill <zhangjia517@hotmail.com>
2020-07-26 16:32:23 +0800

    LM

---
+++ TCommon/LeetCode/TNodeReverse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TCommon.LeetCode
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+    }
+
+    public class TNodeReverse
+    {
+        public static void Execute()
+        {
+            Node list3 = new Node();
+            list3.value = 2;
+
+            Node list2 = new Node();
+            list2.value = 1;
+            list2.next = list3;
+
+            Node list1 = new Node();
+            list1.value = 0;
+            list1.next = list2;
+
+            NodePrint(list1);
+            list1 = Reverse(list1, list1.next);
+            NodePrint(list1);
+        }
+
+        private static void NodePrint(Node list1)
+        {
+            Node n = list1;
+            while (n != null)
+            {
+                Console.WriteLine(n.value);
+                n = n.next;
+            }
+        }
+
+        private static Node Reverse(Node current, Node next)
+        {
+            Node tmp = next.next;
+            next.next = current;
+            if (current.value == 0)
+            {
+                current.next = null;
+            }
+            if (tmp == null)
+            {
+                return next;
+            }
+            else
+            {
+                return Reverse(next, tmp);
+            }
+        }
+    }
+}
---
+++ TCommon/LeetCode/TSplitChar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TCommon.LeetCode
+{
+    public class TSplitChar
+    {
+        public static void Execute()
+        {
+            List<string> strList = new List<string>();
+            strList = SplitChar("a:bb:ccc", ':');
+            for (int i = 0; i < strList.Count; i++)
+            {
+                Console.WriteLine(strList[i]);
+            }
+        }
+
+        private static List<string> SplitChar(string str1, char c1)
+        {
+            List<string> strList = new List<string>();
+            string tempStr = "";
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] == c1)
+                {
+                    strList.Add(tempStr);
+                    tempStr = "";
+                }
+                else
+                {
+                    tempStr += str1[i];
+                }
+            }
+            strList.Add(tempStr);
+            return strList;
+        }
+    }
+}
